0f07fa78ff17e928e746d06b877df5a7c46ea459
yeburge <55379169+yeburge@users.noreply.github.com>
2019-11-04 10:34:30 +0200

    New update(Added new functions)
    New update

--- neury.py
+++ neury.py
@@ -1,4 +1,4 @@
-#Вики Voice manager v1
+#Вики Voice manager v1.5(Новый Оброзец!!!!!!!)
 import os
 import os.path
 import time
@@ -25,12 +25,8 @@ opts = {
         "facts": ('расскажи факты','интересные факты','факты'),
         "weather": ('скажи погоду', 'какая погода', 'погода', 'погада', 'погуда', 'какая погуда'),
         "filework": ('открой документ', 'дакумент', 'окрыть документ', 'документ'),
-        "search": ('начать поиск', 'поиск', 'искать')
-    },
-    "acmd": {
-        "acmdfile": ('закрыть файл', 'закрытие файла', 'зокрой файл', 'зокрыть файл')
-
-
+        "search": ('начать поиск', 'поиск', 'искать'),
+        "openexe": ('открыть файл', 'открой файл', 'файл', 'фойл', 'фуйл')
     }
 }
  
@@ -77,24 +73,18 @@ def recognize_cmd(cmd):
    
     return RC
 
-def open(path):
-	if platform.system() == "Windows":
-		os.startfile(path)
-	elif platform.system() == "Darwin":
-		subprocess.Popen(["open", path])
-	else:
-		subprocess.Popen(["xdg-open", path])
- 
 def execute_cmd(cmd):
     if cmd == 'ctime':
         # сказать текущее время
         now = datetime.datetime.now()
         speak("Сейчас " + str(now.hour) + ":" + str(now.minute))
+        stop_listening
    
     elif cmd == 'facts':
         # рассказать анекдот
         facts = ['Гепард может развивать скорость 120 километров в час', 'В Средние века листы для рукописей делали из шкур животных', 'Нельзя кипятить воду больше одного раза. При повторном кипячении выделяется диоксин, ядовитое вещество, вызывающее рак', 'Ученые доказали, что 90% болезней возникают от стресса', 'Яд скорпиона - самая дорогая жидкость в мире', 'Пчёл и ос можно научить искать взрывчатые вещества и наркотики, как собак']
         speak(random.choice(facts))
+        stop_listening
     elif cmd == 'weather':
         print("В каком городе мне узнать погоду?")
         weath = r.listen(m)
@@ -107,19 +97,33 @@ def execute_cmd(cmd):
 
         temp = w.get_temperature('celsius')["temp"]
 
-        speak("В городе " + city + ": " + w.get_detailed_status() + str(temp) )
-    #elif cmd == 'filework':
-    	#voice = recognizer.recognize_google(audio, language = "ru-RU").lower()
-    	#acmd = voice
-    	#my_file = "speech.txt"
-    	#if os.path.isfile(my_file) == True:
-    		#my_file = open('speech.txt', 'a')
-    		#my_file.write(voice)
-    	#else:
-    		#my_file = open('speech.txt', 'w')
-    		#my_file.write(voice)
-    	#acmd = recognize_acmd(acmd)
-    	#execute_acmd(acmd["acmd"])
+        print("В городе " + city + ": " + w.get_detailed_status() + str(temp) )
+        if temp < 15:
+        	speak("Температура на улице ниже 15 градусов. Советую одеться потеплее")
+        	stop_listening
+        if temp < 25:
+        	speak("Температура в районе " + str(temp) +  " градусов тепла. Сегодня можно одеть только ветровку")
+        	stop_listening
+        if temp > 25:
+        	speak("Температура на улице больше 25 градусов. Можно одеть шорты и футболку")
+        	stop_listening
+    elif cmd == 'filework':
+    	sound = r.listen(m)
+    	voice = r.recognize_google(sound, language = "ru-RU").lower()
+    	my_file = "C:/RecordedFile/speech.txt"
+    	print("[log] Говорите...")
+    	if os.path.isfile(my_file) == True:
+    		my_file = open('C:/RecordedFile/speech.txt', 'a')
+    		print("[log] Вы сказали: " + voice)
+    		my_file.write(voice)
+    		my_file.close()
+    		stop_listening
+    	else:
+    		my_file = open('C:/RecordedFile/speech.txt', 'w')
+    		print("[log] Вы сказали: " + voice)
+    		my_file.write(voice)
+    		my_file.close()
+    		stop_listening
     elif cmd == 'search':
     	print("Скажите запрос")
     	voice = r.listen(m)
@@ -127,23 +131,19 @@ def execute_cmd(cmd):
     	url = "https://www.google.com/search?q="+search
     	webbrowser.open(url)
     	speak("Вот, что мне удалось найти")
-    else:
-        speak('Вот, что мне удалось найти в интернете по данному запросу')
+    	stop_listening
+    elif cmd == 'openexe':
+    	print("[log] Внимание! Ярлыки файлов должны быть в C:/Program Files/programs (!!! нижний регистр !!!)")
+    	print("Скажите название файла(без рассширения)")
+    	voice = r.listen(m)
+    	exe = r.recognize_google(voice, language="en-EN").lower()
+    	print("[log] Открытие " + exe +".exe")
+    	os.startfile("C:/programs/" + exe + ".lnk")
+    	stop_listening
 
-def recognize_acmd(acmd):
-    RC = {'acmd': '', 'percent': 0}
-    for c,v in opts['cmds'].items():
- 
-        for x in v:
-            vrt = fuzz.ratio(cmd, x)
-            if vrt > RC['percent']:
-                RC['acmd'] = c
-                RC['percent'] = vrt
-   
-    return RC
-def execute_acmd(acmd, my_file):
-	if acmd == 'acmdfile':
-		my_file.close 
+    else:
+        speak('Извините, я не расслышала, что вы сказали')
+        stop_listening
 
 # запуск
 r = sr.Recognizer()
